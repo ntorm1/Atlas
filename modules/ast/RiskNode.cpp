612092edc3496abb964d7558093838bdb95bdd9e
ntorm1 <nathan.tormaschy@gmail.com>
2024-01-21 20:55:29 -0500

    fixed window lookback

--- modules/ast/RiskNode.cpp
+++ modules/ast/RiskNode.cpp
@@ -20,11 +20,9 @@ AllocationWeightNode::~AllocationWeightNode() noexcept
 
 //============================================================================
 AllocationWeightNode::AllocationWeightNode(
-	Time::TimeOffset l,
 	SharedPtr<TriggerNode> trigger
 ) noexcept :
 	OpperationNode<void, LinAlg::EigenVectorXd&>(NodeType::ALLOC_WEIGHT),
-	m_lookback(l),
 	m_trigger(trigger),
 	m_exchange(trigger->getExchange())
 {
@@ -39,44 +37,14 @@ InvVolWeight::~InvVolWeight() noexcept
 
 //============================================================================
 InvVolWeight::InvVolWeight(
-	Time::TimeOffset l,
+	size_t window,
 	SharedPtr<TriggerNode> trigger
 ) noexcept :
-	AllocationWeightNode(l, trigger)
+	AllocationWeightNode(trigger),
+	m_lookback_window(window)
 {
 	m_vol.resize(m_exchange.getAssetCount());
 	m_vol.setZero();
-}
-
-
-//============================================================================
-void
-AllocationWeightNode::buildLookbackDefs() noexcept
-{
-	Eigen::VectorXi const& mask = m_trigger->getMask();
-	Exchange const& exchange = m_trigger->getExchange();
-	auto const& timestamps = exchange.getTimestamps();
-
-	assert(mask.size() == timestamps.size());
-	m_lookback_defs.reserve(mask.size());
-
-	for (size_t i = 0; i < timestamps.size(); ++i)
-	{
-		if (mask[i] == 0)
-		{
-			continue;
-		}
-		auto timestamp = timestamps[i];
-		auto timestamp_start = applyTimeOffset(timestamp, m_lookback);
-		auto it = std::lower_bound(
-			timestamps.begin() + i,
-			timestamps.end(),
-			timestamp_start
-		);
-		assert(it != timestamps.end());
-		size_t lookback_start = it - timestamps.begin();
-		m_lookback_defs.emplace_back(i, lookback_start, i);
-	}
 }
 
 
@@ -87,11 +55,12 @@ InvVolWeight::cache() noexcept
 	// pull out the block of returns data we need using the lookback defs
 	// to determine the start and end indices. Lookback returns block has #rows 
 	// equal to the number of assets, with columnds for timestamps.
-	assert(m_lookback_idx < m_lookback_defs.size());
-	RiskLookbackDef const& def = m_lookback_defs[m_lookback_idx];
+	size_t exchange_idx = m_exchange.currentIdx();
+	assert(exchange_idx > m_lookback_window);
+	size_t start_idx = exchange_idx - m_lookback_window;
 	auto returns_block = m_exchange.getMarketReturnsBlock(
-		def.returns_start_idx,
-		def.returns_end_idx
+		start_idx,
+		exchange_idx
 	);
 	for (size_t i = 0; i < m_exchange.getAssetCount(); ++i)
 	{
--- modules/ast/RiskNode.ixx
+++ modules/ast/RiskNode.ixx
@@ -15,46 +15,21 @@ namespace AST
 {
 
 
-
-//============================================================================
-struct RiskLookbackDef
-{
-	size_t trigger_call_idx;
-	size_t returns_start_idx;
-	size_t returns_end_idx;
-
-	RiskLookbackDef(
-		size_t trigger_call_idx,
-		size_t returns_start_idx,
-		size_t returns_end_idx
-	) noexcept :
-		trigger_call_idx(trigger_call_idx),
-		returns_start_idx(returns_start_idx),
-		returns_end_idx(returns_end_idx)
-	{}
-};
-
-
 //============================================================================
 export class AllocationWeightNode : public OpperationNode<void, LinAlg::EigenVectorXd&>
 {
 protected:
-	Time::TimeOffset m_lookback;
 	SharedPtr<TriggerNode> m_trigger;
-	Vector<RiskLookbackDef> m_lookback_defs;
 	Exchange const& m_exchange;
-	void buildLookbackDefs() noexcept;
 
 public:
 	virtual ~AllocationWeightNode() noexcept;
 	AllocationWeightNode(
-		Time::TimeOffset l,
 		SharedPtr<TriggerNode> trigger
 	) noexcept;
 
 	virtual void cache() noexcept = 0;
 	void evaluate(LinAlg::EigenVectorXd& target) noexcept override = 0;
-
 };
 
 
@@ -63,11 +38,11 @@ export class InvVolWeight final : public AllocationWeightNode
 {
 private:
 	LinAlg::EigenVectorXd m_vol;
-	size_t m_lookback_idx = 0;
+	size_t m_lookback_window = 0;
 public:
 	~InvVolWeight() noexcept;
 	InvVolWeight(
-		Time::TimeOffset l,
+		size_t lookback_window,
 		SharedPtr<TriggerNode> trigger
 	) noexcept;
 
