0fd79e0f934057ac572d3c75d7898a48fb55a0c8
ntorm1 <nathan.tormaschy@gmail.com>
2024-03-13 19:16:07 -0400

    parser

--- AtlasPy/AtlasWrap/parser.py
+++ AtlasPy/AtlasWrap/parser.py
@@ -7,20 +7,35 @@ import tomllib
 from AtlasPy.core import Hydra
 
 class Parser:
-    _hydra: Hydra
-    _toml: Dict[str, Any]
+    _hydra: Hydra = None
+    _hydra_config_dir: str = None
+    _toml: Dict[str, Any] = None
 
     def __init__(
             self,
-            hydra: Hydra
+            hydra_config_dir: Hydra
         ) -> None:
-        self._hydra = hydra
+        self._hydra_config_dir = hydra_config_dir
+        self._hydra = Hydra()
+        self._parse()
 
-    def parse(
+    def getHydra(
+            self
+            ) -> Hydra:
+        return self._hydra
+
+    def _parse(
             self,
-            file_path: str,
-            ) -> Dict[str, Any]:
-        self._load_toml(file_path)
+            ) -> None:
+        if not os.path.isdir(self._hydra_config_dir):
+            raise FileNotFoundError(f"expected dir: {self._hydra_config_dir}")           
+
+        _hydra_config_path = os.path.join(self._hydra_config_dir, "Hydra.toml")
+
+        if not os.path.isfile(_hydra_config_path):
+            raise FileNotFoundError(f"missing Hydra.toml: {_hydra_config_path}")
+        
+        self._load_toml(_hydra_config_path)
         self._validate_toml()
 
     def _load_toml(
@@ -34,44 +49,58 @@ class Parser:
             logging.error(f"An error occurred: {type(e).__name__}: {e}")
             raise e
         
-    def _validate_toml(
-            self,
-            ) -> None:
-        self._validate_strategy()
-        self._validate_portfolio()
+        self._load_exchanges()
+        self._load_portfolios()
 
-    def _validate_strategy(self) -> None:
-        strategy = self._toml.get('strategy')
-
-        if not strategy or not isinstance(strategy, dict):
-            logging.error("Invalid strategy format in the TOML file.")
-            raise ValueError("Invalid strategy format in the TOML file.")
-
-        strategy_id = strategy.get('id')
-
-        if not strategy_id or not isinstance(strategy_id, str):
-            logging.error("Invalid strategy ID format.")
-            raise ValueError("Invalid strategy ID format.")
-        return None
-    
-    def _validate_portfolio(
+    def _load_portfolios(
             self
             ) -> None:
-        portfolio = self._toml.get('portfolio')
+        if not "portfolios" in self._toml or not isinstance(self._toml["portfolios"], list):
+            raise ValueError("""missing portfolios in Hydra.toml, expected: 
+                    [portfolios]
+                    [[portfolios.ids]]
+                    id = "test"
+                    exchange_id = "test"
+                    starting_cash = 100.0
+                """
+            )
+        
+        for portfolio in self._toml["portfolios"]:
+            if not isinstance(portfolio, dict):
+                raise ValueError("expected portfolio to be a dict")
+            id = portfolio.get("id")
+            exchange_id = portfolio.get("exchange_id")
+            starting_cash = portfolio.get("starting_cash")
+            if not id or not exchange_id or not starting_cash:
+                raise ValueError("portfolio missing id, exchange_id, or starting_cash")
+            exchange = self._hydra.getExchange(exchange_id)
+            self._hydra.addPortfolio(id, exchange, starting_cash)
 
-        if not portfolio or not isinstance(portfolio, dict):
-            logging.error("Invalid portfolio format in the TOML file.")
-            raise ValueError("Invalid portfolio format in the TOML file.")
+    def _load_exchanges(
+            self
+            ) -> None:
+        if not "exchanges" in self._toml or not isinstance(self._toml["exchanges"], list):
+            raise ValueError("""missing exchanges in Hydra.toml, expected: 
+                    [exchanges]
+                    [[exchanges.ids]]
+                    id = "test"
+                    path = "C:/Users/.../exchange1"
+                    datetime_format = "%Y-%m-%d %H:%M:%S"
+                """
+            )
+        
+        for exchange in self._toml["exchanges"]:
+            if not isinstance(exchange, dict):
+                raise ValueError("expected exchange to be a dict")
+            id = exchange.get("id")
+            path = exchange.get("path")
+            datetime_format = exchange.get("datetime_format")
+            if not id or not path or not datetime_format:
+                raise ValueError("exchange missing id, path, or datetime_format")
+            self._hydra.addExchange(id, path, datetime_format)
+        
 
-        portfolio_id = portfolio.get('id')
-
-        if not portfolio_id or not isinstance(portfolio_id, str):
-            logging.error("Invalid portfolio ID format.")
-            raise ValueError("Invalid portfolio ID format.")
-    
-        try:
-            portfolio = self._hydra.getPortfolio(portfolio_id)
-        except Exception as e:
-            logging.error(f"An error occurred: {type(e).__name__}: {e}")
-            raise e
-        return None
+    def _validate_toml(
+            self
+            ) -> None:
+        pass
--- AtlasPy/tests/test_parser.py
+++ AtlasPy/tests/test_parser.py
@@ -5,26 +5,27 @@ import context
 
 import AtlasWrap
 
-TEST_FILE_DIR = "files"
+HYDRA_DIR = "files/hydra1"
 TEST_FILE_1 = "test_strategy_1.toml"
 PORTFOLIO_ID = "test_portfolio_1"
 STRATEGY_ID = "test_strategy_1"
 EXCHANGE_ID = "test_exchange_1"
+
 EXCHANGE_CSV = r"C:/Users/natha/OneDrive/Desktop/C++/Atlas/AtlasPy/src/exchange1"
 
 
 class TestParser(unittest.TestCase):
     def setUp(self) -> None:
-        self.hydra = AtlasWrap.Hydra()
-        self.exchange = self.hydra.addExchange(EXCHANGE_ID, EXCHANGE_CSV,  "%Y-%m-%d")
-        self.portfolio = self.hydra.addPortfolio(PORTFOLIO_ID, self.exchange, 100.0)
+        hydra_path = os.path.join(os.path.dirname(__file__),HYDRA_DIR)
+        parser = AtlasWrap.Parser(hydra_path)
+        self.hydra = parser.getHydra()
 
-    def get_full_path(self, file_name):
-        return os.path.join(os.path.dirname(__file__),TEST_FILE_DIR, file_name)
+        #self.exchange = self.hydra.addExchange(EXCHANGE_ID, EXCHANGE_CSV,  "%Y-%m-%d")
+        #self.portfolio = self.hydra.addPortfolio(PORTFOLIO_ID, self.exchange, 100.0)
 
     def test_parse(self):
-        parser = AtlasWrap.Parser(self.hydra)
-        parser.parse(self.get_full_path(TEST_FILE_1))
+        self.hydra.getExchange(EXCHANGE_ID)
+        self.hydra.getPortfolio(PORTFOLIO_ID)
         self.assertTrue(True)
     
 if __name__ == '__main__':
